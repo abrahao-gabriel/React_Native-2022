78d2c15948969ff8ef39b8cdf21a7bcba397dea9
abrahao-gabriel <57734187+abrahao-gabriel@users.noreply.github.com>
2022-10-01 14:40:46 -0300

    Add files via upload

---
+++ app05/styles.js
@@ -0,0 +1,40 @@
+import {StyleSheet} from 'react-native';
+ 
+const styles = StyleSheet.create({
+    container:{
+      flex: 1,
+      padding: 30,
+    },
+    texto:{
+      textAlign: 'center',
+      fontSize: 30,
+      marginTop: 30,
+      marginBottom: 30,
+    },
+    resultado:{
+      textAlign: 'center',
+      fontSize: 30,
+      marginTop: 30,
+    },
+    input:{
+        height: 45,
+        borderWidth: 1,
+        borderColor: 'white',
+        margin: 10,
+        fontSize: 20,
+        padding: 10,
+      },
+    img:{
+      width: 350, 
+      height: 350, 
+      alignSelf: 'center', 
+      marginTop:10,
+      marginBottom:10
+  },
+  teste:{
+    color: "white"
+  }
+  
+  });
+ 
+  export {styles};
